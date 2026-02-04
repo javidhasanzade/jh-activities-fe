1010f6a38331675118327f580b27a9c478b90f14
Javid Hasanzade <J.Hasanzade@fp-dbs.com>
2026-02-04 16:44:44 +0100

    added material ui and axios, made localhost https

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import {useEffect, useState} from "react";
+import {List, ListItem, ListItemText, Typography} from "@mui/material";
+import axios from "axios";
 
 function App() {
     const [activities, setActivities] = useState<Activity[]>([]);
 
     useEffect(() => {
-        fetch('https://localhost:5001/api/activities')
-            .then(response => response.json())
-            .then(data => setActivities(data));
+        axios.get<Activity[]>('https://localhost:5001/api/activities')
+            .then(response => setActivities(response.data));
     }, []);
 
     return (
-        <div>
-            <h3>Reactivities</h3>
-            <ul>
+        <>
+            <Typography variant='h3'>Reactivities</Typography>
+            <List>
                 {activities.map(activity => (
-                    <li key={activity.id}>{activity.title}</li>
+                    <ListItem key={activity.id}>
+                        <ListItemText>{activity.title}</ListItemText>
+                    </ListItem>
                 ))}
-            </ul>
-        </div>
+            </List>
+        </>
     )
 }
 
