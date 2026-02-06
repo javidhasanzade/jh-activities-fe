d2a63801c40c1734fc1ab09a787547f23a147793
javidhasanzade <jh.grunger@gmail.com>
2026-02-06 13:29:09 +0100

    selecting an individual activity to view

--- src/features/activities/dashboard/ActivityList.tsx
+++ src/features/activities/dashboard/ActivityList.tsx
@@ -2,14 +2,15 @@ import {Box} from "@mui/material";
 import {ActivityCard} from "./ActivityCard.tsx";
 
 type Props = {
-    activities: Activity[]
+    activities: Activity[];
+    selectActivity: (id: string) => void;
 }
 
-export const ActivityList = ({activities}: Props) => {
+export const ActivityList = ({activities, selectActivity}: Props) => {
     return (
         <Box sx={{display: 'flex', flexDirection: 'column', gap: 3}}>
             {activities.map(activity => (
-                <ActivityCard activity={activity} key={activity.id}/>
+                <ActivityCard activity={activity} key={activity.id} selectActivity={selectActivity}/>
             ))}
         </Box>
     );
